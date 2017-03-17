a0732a5b747197c3e621685f72e466b64033ef58
Wisnu Adi Nurcahyo <wisnu@nurcahyo.me>
2017-03-17 20:39:39 +0700

    Initial commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
+import System.Environment (getArgs)
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+
+  if (null args)
+    then display help
+    else do
+      let command = head args
+
+      case command of "help"      -> display help
+                      "-h"        -> display help
+                      "--help"    -> display help
+                      "about"     -> display about
+                      "version"   -> display appVersion
+                      "-v"        -> display appVersion
+                      "--version" -> display appVersion
+                      otherwise -> run args
